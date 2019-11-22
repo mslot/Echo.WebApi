ad5ec1d3bfba0f689cd6c94e2e1fc68abed28975
Martin Slot <mas@impact.dk>
2019-11-22 22:25:26 +0100

    Add project files.

---
+++ Echo.WebApi/Controllers/EchoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Echo.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EchoController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get(string say)
+        {
+            if (String.IsNullOrEmpty(say))
+                return Ok("hello");
+            return Ok(say);
+        }
+    }
+}
