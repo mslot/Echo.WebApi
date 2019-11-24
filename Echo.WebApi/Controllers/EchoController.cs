9a581719621f261c3271b7eb8777cb420432e544
Martin Slot <mas@impact.dk>
2019-11-24 23:45:35 +0100

    Removed test stuff

--- Echo.WebApi/Controllers/EchoController.cs
+++ Echo.WebApi/Controllers/EchoController.cs
@@ -8,15 +8,15 @@ namespace Echo.WebApi.Controllers
     [Route("[controller]")]
     public class EchoController : ControllerBase
     {
-        static int c = 0;
+        //static int c = 0;
         [HttpGet]
         public IActionResult Get(string say)
         {
-            if (c == 0)
-            {
-                Thread.Sleep(TimeSpan.FromMinutes(1));
-                c++;
-            }
+            //if (c == 0)
+            //{
+            //    Thread.Sleep(TimeSpan.FromMinutes(1));
+            //    c++;
+            //}
             if (String.IsNullOrEmpty(say))
                 return Ok("hello");
             return Ok(say);
