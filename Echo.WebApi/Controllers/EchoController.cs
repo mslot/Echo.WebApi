115370984687b2e2c52013ba43995b5085ab931f
Martin Slot <mas@impact.dk>
2019-11-25 22:17:41 +0100

    Removed test settings

--- Echo.WebApi/Controllers/EchoController.cs
+++ Echo.WebApi/Controllers/EchoController.cs
@@ -8,17 +8,11 @@ namespace Echo.WebApi.Controllers
     [Route("[controller]")]
     public class EchoController : ControllerBase
     {
-        //static int c = 0;
         [HttpGet]
         public IActionResult Get(string say)
         {
-            //if (c == 0)
-            //{
-            //    Thread.Sleep(TimeSpan.FromMinutes(1));
-            //    c++;
-            //}
             if (String.IsNullOrEmpty(say))
-                return Ok("hello world");
+                return Ok("hello");
             return Ok(say);
         }
     }
